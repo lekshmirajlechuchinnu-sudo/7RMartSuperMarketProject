e9ae4e4514a1cf569716925c56d44651c1255ded
gokul <gokul@gokul>
2026-07-15 12:20:43 +0530

    15-07-2026

--- 7RMartSuperMarket/src/main/java/constants/Constant.java
+++ 7RMartSuperMarket/src/main/java/constants/Constant.java
@@ -1,6 +1,8 @@
 package constants;
 
 public class Constant {
-	public static final String TESTDATA=System.getProperty("user.dir")+"\\src\\test\\resources\\TestData.xlsx";
-	public static final String IMAGE=System.getProperty("user.dir")+"\\src\\test\\resources\\vegetable.jpg";
+	public static final String TESTDATA = System.getProperty("user.dir") + "\\src\\test\\resources\\TestData.xlsx";
+	public static final String IMAGE = System.getProperty("user.dir") + "\\src\\test\\resources\\vegetable.jpg";
+
+	public static final String CONFIGFILE = System.getProperty("src\\main\\resources\\config.properties");
 }
--- 7RMartSuperMarket/src/test/java/testscript/Base.java
+++ 7RMartSuperMarket/src/test/java/testscript/Base.java
@@ -1,7 +1,10 @@
 package testscript;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,43 +14,49 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import constants.Constant;
 import utilities.ScreenShotUtility;
 import utilities.WaitUtility;
 
 public class Base {
 
 	public WebDriver driver;
+	Properties pr;
+	FileInputStream fileinput;
 
-	@BeforeMethod(alwaysRun=true)
+	@BeforeMethod(alwaysRun = true)
 	@Parameters("Browser")
-	
+
 	public void initializedriver(String browser) throws Exception {
-			if(browser.equalsIgnoreCase("chrome"))
-			{
+		try {
+
+			pr = new Properties();
+			fileinput = new FileInputStream(Constant.CONFIGFILE);
+			pr.load(fileinput);// for reading object
+
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		if (browser.equalsIgnoreCase("chrome")) {
 			driver = new ChromeDriver();
-			}
-			else if(browser.equalsIgnoreCase("firefox"))
-			{
-				driver=new FirefoxDriver();
-			}
-			else
-			{
-				throw new Exception("Invalid browser");
-			}
-			
-		//driver=new ChromeDriver();
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			throw new Exception("Invalid browser");
+		}
+
+		// driver=new ChromeDriver();
+		// driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		driver.get(pr.getProperty("url"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitUtility.IMPLICITWAIT));
 		driver.manage().window().maximize();
-		// driver.navigate().to("https://groceryapp.uniqassosiates.com/admin");
-		
-		
 
-		
 	}
-	
+
 	@AfterMethod
-	
+
 	public void browserQuit(ITestResult iTestResult) throws IOException {
 		if (iTestResult.getStatus() == ITestResult.FAILURE) {
 			ScreenShotUtility scrShot = new ScreenShotUtility(); // creating obj
@@ -55,5 +64,5 @@ public class Base {
 		}
 		driver.quit();
 
-}
+	}
 }
