ae66f9296015a038af2f5a3f776a7a3121e3e461
gokul <gokul@gokul>
2026-07-15 12:23:18 +0530

    15-07-2026

--- 7RMartSuperMarket/src/main/java/constants/Constant.java
+++ 7RMartSuperMarket/src/main/java/constants/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
 	public static final String TESTDATA = System.getProperty("user.dir") + "\\src\\test\\resources\\TestData.xlsx";
 	public static final String IMAGE = System.getProperty("user.dir") + "\\src\\test\\resources\\vegetable.jpg";
 
-	public static final String CONFIGFILE = System.getProperty("src\\main\\resources\\config.properties");
+	public static final String CONFIGFILE = System.getProperty("user.dir") + "\\src\\main\\resources\\config.properties";
 }
